1961c1bbd598dddd22a07906a08cf609038e7825
Radek Chaloupka <wpeasy@gmail.com>
2026-02-06 11:23:50 +0100

    Pre form seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolesAndPermissionsSeeder::class,
             AdminUserSeeder::class,
+            FormsSeeder::class,
         ]);
     }
 }
